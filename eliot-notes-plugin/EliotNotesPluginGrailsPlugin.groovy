f29832830de570fc982d2986bd0a344ff6e05975
ticetime <github@ticetime.com>
2011-07-08 10:28:11 +0200

    #8 Découpage en plugins fonctionnel Création plugin pour la gestion des notes avec classes du domaine

--- eliot-notes-plugin/EliotNotesPluginGrailsPlugin.groovy
+++ eliot-notes-plugin/EliotNotesPluginGrailsPlugin.groovy
@@ -26,22 +26,24 @@
  *  <http://www.cecill.info/licences.fr.html>.
  */
 
-class EliotTicePluginGrailsPlugin {
+class EliotNotesPluginGrailsPlugin {
   // the plugin version
   def version = "0.1-SNAPSHOT"
   // the version or versions of Grails the plugin is designed for
   def grailsVersion = "1.4 > *"
   // the other plugins this plugin depends on
-  def dependsOn = [:]
+  def dependsOn = ['eliot-tice-plugin':'0.1-SNAPSHOT > *']
   // resources that are excluded from plugin packaging
   def pluginExcludes = [
           "grails-app/views/error.gsp"
   ]
 
-  def title = "Eliot Tice Plugin"
-  def author = "Franck Silvestre"
+  def title = "Eliot Notes  Plugin" // Headline display name of the plugin
+  def author = "Franck Silvestre - Fylab"
   def authorEmail = ""
   def description = '''\
-Plugin pour la création d'applications Eliot
+Plugin contenant les services métiers relatifs à la gestion des notes"
 '''
+
+
 }
