e3a4fbec2b520183ea359b2c6234882cb0c5c46d
ticetime <github@ticetime.com>
2011-10-22 12:15:19 +0200

    #52: Support type question Choix exclusif [partiel] Mise en place Services

---
+++ eliot-tdbase/grails-app/services/org/lilie/services/eliot/tdbase/impl/exclusivechoice/QuestionExclusiveChoiceSpecificationService.groovy
@@ -0,0 +1,169 @@
+/*
+ * Copyright © FYLAB and the Conseil Régional d'Île-de-France, 2009
+ * This file is part of L'Interface Libre et Interactive de l'Enseignement (Lilie).
+ *
+ * Lilie is free software. You can redistribute it and/or modify since
+ * you respect the terms of either (at least one of the both license) :
+ * - under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * - the CeCILL-C as published by CeCILL-C; either version 1 of the
+ * License, or any later version
+ *
+ * There are special exceptions to the terms and conditions of the
+ * licenses as they are applied to this software. View the full text of
+ * the exception in file LICENSE.txt in the directory of this software
+ * distribution.
+ *
+ * Lilie is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Licenses for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * and the CeCILL-C along with Lilie. If not, see :
+ *  <http://www.gnu.org/licenses/> and
+ *  <http://www.cecill.info/licences.fr.html>.
+ */
+
+
+
+
+
+package org.lilie.services.eliot.tdbase.impl.exclusivechoice
+
+import groovy.json.JsonBuilder
+import groovy.json.JsonSlurper
+import org.lilie.services.eliot.tdbase.QuestionSpecificationService
+import org.lilie.services.eliot.tice.utils.StringUtils
+import org.lilie.services.eliot.tdbase.Question
+import org.gcontracts.annotations.Requires
+
+/**
+ *
+ * @author franck Silvestre
+ */
+class QuestionExclusiveChoiceSpecificationService implements QuestionSpecificationService {
+
+  static transactional = false
+
+
+  /**
+   *
+   * @see QuestionSpecificationService
+   */
+  def getObjectFromSpecification(String specification) {
+    if (!specification) {
+      return new ExclusiveChoiceSpecification()
+    }
+    def slurper = new JsonSlurper()
+    Map map = slurper.parseText(specification)
+    return new ExclusiveChoiceSpecification(map)
+  }
+
+  /**
+   *
+   * @see QuestionSpecificationService
+   */
+  @Requires({object instanceof ExclusiveChoiceSpecification})
+  String getSpecificationFromObject(Object object) {
+    JsonBuilder builder = new JsonBuilder(object.toMap())
+    return builder.toString()
+  }
+
+  /**
+   *
+   * @see QuestionSpecificationService
+   */
+  @Requires({object instanceof ExclusiveChoiceSpecification})
+  String getSpecificationNormaliseFromObject(Object object) {
+    String toNormalise = object.libelle
+    if (toNormalise) {
+      return StringUtils.normalise(toNormalise)
+    }
+    return null
+  }
+
+  /**
+   *
+   * @see QuestionSpecificationService
+   */
+  def updateQuestionSpecificationForObject(Question question, Object object) {
+    question.specification = getSpecificationFromObject(object)
+    question.specificationNormalise = getSpecificationNormaliseFromObject(object)
+    question.save()
+  }
+
+}
+
+/**
+ * Représente un objet spécification pour une question de type MultipleChoice
+ */
+class ExclusiveChoiceSpecification {
+  String libelle
+  String correction
+  List<ExclusiveChoiceSpecificationReponsePossible> reponses = []
+
+  ExclusiveChoiceSpecification() {
+    super()
+  }
+
+  /**
+   * Créer et initialise un nouvel objet de type ExclusiveChoiceSpecification
+   * @param map la map permettant d'initialiser l'objet en cours
+   * de création
+   */
+  ExclusiveChoiceSpecification(Map map) {
+    libelle = map.libelle
+    correction = map.correction
+    reponses = map.reponses.collect {
+      if (it instanceof ExclusiveChoiceSpecificationReponsePossible) {
+        it
+      } else {
+        new ExclusiveChoiceSpecificationReponsePossible(it)
+      }
+    }
+  }
+
+  def toMap() {
+    [
+            libelle: libelle,
+            correction: correction,
+            reponses: reponses*.toMap()
+    ]
+  }
+
+}
+
+/**
+ * Représente un objet réponse possible de la spécification  pour une question de
+ * type MultipleChoice
+ */
+class ExclusiveChoiceSpecificationReponsePossible {
+  String libelleReponse
+  boolean estUneBonneReponse
+  Float rang
+
+  ExclusiveChoiceSpecificationReponsePossible() {
+    super()
+  }
+
+  /**
+   * Créer et initialise un nouvel objet de type ExclusiveChoiceSpecificationReponsePossible
+   * @param map la map permettant d'initialiser l'objet en cours
+   * de création
+   */
+  ExclusiveChoiceSpecificationReponsePossible(Map map) {
+    libelleReponse = map.libelleReponse
+    estUneBonneReponse = map.estUneBonneReponse
+    rang = map.rang
+  }
+
+  def toMap() {
+    [
+            libelleReponse: libelleReponse,
+            estUneBonneReponse: estUneBonneReponse,
+            rang: rang
+    ]
+  }
+}
---
+++ eliot-tdbase/grails-app/services/org/lilie/services/eliot/tdbase/impl/exclusivechoice/ReponseExclusiveChoiceSpecificationService.groovy
@@ -0,0 +1,191 @@
+/*
+ * Copyright © FYLAB and the Conseil Régional d'Île-de-France, 2009
+ * This file is part of L'Interface Libre et Interactive de l'Enseignement (Lilie).
+ *
+ * Lilie is free software. You can redistribute it and/or modify since
+ * you respect the terms of either (at least one of the both license) :
+ * - under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * - the CeCILL-C as published by CeCILL-C; either version 1 of the
+ * License, or any later version
+ *
+ * There are special exceptions to the terms and conditions of the
+ * licenses as they are applied to this software. View the full text of
+ * the exception in file LICENSE.txt in the directory of this software
+ * distribution.
+ *
+ * Lilie is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Licenses for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * and the CeCILL-C along with Lilie. If not, see :
+ *  <http://www.gnu.org/licenses/> and
+ *  <http://www.cecill.info/licences.fr.html>.
+ */
+
+
+
+
+
+
+
+package org.lilie.services.eliot.tdbase.impl.exclusivechoice
+
+import groovy.json.JsonBuilder
+import groovy.json.JsonSlurper
+import org.gcontracts.annotations.Requires
+import org.lilie.services.eliot.tdbase.Question
+import org.lilie.services.eliot.tdbase.Reponse
+import org.lilie.services.eliot.tdbase.ReponseSpecificationService
+import org.lilie.services.eliot.tdbase.impl.multiplechoice.MultipleChoiceSpecificationReponsePossible
+import org.springframework.transaction.annotation.Transactional
+
+/**
+ *
+ * @author franck Silvestre
+ */
+class ReponseExclusiveChoiceSpecificationService implements ReponseSpecificationService {
+
+  static transactional = false
+
+  /**
+   *
+   * @see ReponseSpecificationService
+   */
+  def getObjectFromSpecification(String specification) {
+    if (!specification) {
+      return new ReponseExclusiveChoiceSpecification()
+    }
+    def slurper = new JsonSlurper()
+    Map map = slurper.parseText(specification)
+    return new ReponseExclusiveChoiceSpecification(map)
+  }
+
+  /**
+   *
+   * @see ReponseSpecificationService
+   */
+  @Requires({object instanceof ReponseExclusiveChoiceSpecification})
+  String getSpecificationFromObject(Object object) {
+    ReponseExclusiveChoiceSpecification spec = object
+    JsonBuilder builder = new JsonBuilder(spec.toMap())
+    return builder.toString()
+  }
+
+  /**
+   *
+   * @see ReponseSpecificationService
+   */
+  def updateReponseSpecificationForObject(Reponse reponse, Object object) {
+    reponse.specification = getSpecificationFromObject(object)
+    reponse.save()
+  }
+
+  /**
+   * @see ReponseSpecificationService
+   */
+  def initialiseReponseSpecificationForQuestion(Reponse reponse,
+                                                Question question) {
+    def specObj = getObjectInitialiseFromSpecification(question)
+    updateReponseSpecificationForObject(reponse, specObj)
+  }
+
+  /**
+   * @see ReponseSpecificationService
+   */
+  @Requires({question.specificationObject instanceof ExclusiveChoiceSpecification})
+  def getObjectInitialiseFromSpecification(Question question) {
+    def questSpecObj = question.specificationObject
+    def reponsesPossibles = questSpecObj.reponses
+    ReponseExclusiveChoiceSpecification specObj = new ReponseExclusiveChoiceSpecification()
+    reponsesPossibles.each {
+      specObj.reponses << new ExclusiveChoiceSpecificationReponsePossible(
+              libelleReponse: it.libelleReponse,
+              estUneBonneReponse: false
+      )
+    }
+    return specObj
+  }
+
+  /**
+   * Si il n'y a aucune réponse explicite (pas de bouton coché), la notes est 0.
+   * Si il y a au moins une réponse explicite (un bouton cochée), alors :
+   * - si réponse juste on ajoute un point
+   * - si réponse fausse on retranche un point
+   * On effectue une règle de trois pour ramener la note correspondant au barême
+   *
+   * @see ReponseSpecificationService
+   */
+  @Transactional
+  Float evalueReponse(Reponse reponse) {
+    def res = 0
+    boolean aucuneReponse = true
+    ReponseExclusiveChoiceSpecification repSpecObj = reponse.specificationObject
+    ExclusiveChoiceSpecification questSpecObj = reponse.sujetQuestion.question.specificationObject
+    def nbRepPos = repSpecObj.reponses.size()
+    for (int i = 0; i < nbRepPos; i++) {
+      ExclusiveChoiceSpecificationReponsePossible repPos = repSpecObj.reponses[i]
+      ExclusiveChoiceSpecificationReponsePossible repPosQ = questSpecObj.reponses[i]
+      if (repPos.libelleReponse != repPosQ.libelleReponse) {
+        log.info("Libelles reponses incohérent : ${repPos.libelleReponse} <> ${repPosQ.libelleReponse}")
+      }
+      if (repPos.estUneBonneReponse) {
+        aucuneReponse = false
+        if (repPos.estUneBonneReponse == repPosQ.estUneBonneReponse) {
+          res += 1
+        } else {
+          res += -1
+        }
+      }
+    }
+    if (aucuneReponse) { // si aucune reponse, l'eleve n'a pas repondu, il a 0
+      res = 0
+    } else { // sinon on décompte avec points positifs et/ou négatifs
+      res = (res / nbRepPos) * reponse.sujetQuestion.points
+    }
+    reponse.correctionNoteAutomatique = res
+    reponse.save()
+    return res
+  }
+
+
+}
+
+/**
+ * Représente un objet spécification pour une question de type MultipleChoice
+ */
+class ReponseExclusiveChoiceSpecification {
+
+  List<MultipleChoiceSpecificationReponsePossible> reponses = []
+
+  ReponseExclusiveChoiceSpecification() {
+    super()
+  }
+
+  /**
+   * Créer et initialise un nouvel objet de type RepoonseMultipleChoiceSpecification
+   * @param map la map permettant d'initialiser l'objet en cours
+   * de création
+   */
+  ReponseExclusiveChoiceSpecification(Map map) {
+    reponses = map.reponses.collect {
+      if (it instanceof MultipleChoiceSpecificationReponsePossible) {
+        it
+      } else {
+        new MultipleChoiceSpecificationReponsePossible(it)
+      }
+    }
+  }
+
+
+
+  def toMap() {
+    [
+            reponses: reponses*.toMap()
+    ]
+  }
+
+}
