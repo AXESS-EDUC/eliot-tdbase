fe8ea4406fd01907be3ed07898788e705579e3f4
ticetime <github@ticetime.com>
2012-03-18 12:17:37 +0100

    webhelp enseignants : fonctionnalités essentielles

---
+++ aide/webhelp/Manuel_Utilisateur_TDBase_Enseignant/content/search/htmlFileList.js
@@ -0,0 +1,21 @@
+//List of files which are indexed.
+fl = new Array();
+fl["0"]= "ch01.html";
+fl["1"]= "ch01s01.html";
+fl["2"]= "ch01s02.html";
+fl["3"]= "ch01s03.html";
+fl["4"]= "ch01s04.html";
+fl["5"]= "ch01s05.html";
+fl["6"]= "ch01s06.html";
+fl["7"]= "ch01s07.html";
+fl["8"]= "ch02.html";
+fl["9"]= "ch02s01.html";
+fl["10"]= "ch02s02.html";
+fl["11"]= "ch02s03.html";
+fl["12"]= "ch02s04.html";
+fl["13"]= "ch02s05.html";
+fl["14"]= "ch02s06.html";
+fl["15"]= "ch03.html";
+fl["16"]= "ch03s01.html";
+fl["17"]= "ch03s02.html";
+fl["18"]= "ch03s03.html";
