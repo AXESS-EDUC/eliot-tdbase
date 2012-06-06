b6858538e0ea90592af41f7153d79c00e5500d4e
ticetime <github@ticetime.com>
2012-06-06 20:20:26 +0200

    #156 Livraison 2.0.1-B1 Passage en version 2.0.1-B1

--- eliot-tice-plugin/grails-app/conf/BuildConfig.groovy
+++ eliot-tice-plugin/grails-app/conf/BuildConfig.groovy
@@ -59,7 +59,7 @@ grails.project.dependency.resolution = {
     // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
     runtime "postgresql:postgresql:8.4-702.jdbc4"
-    compile group: 'org.liquibase', name: 'liquibase-core', version: '2.0.1'
+    compile group: 'org.liquibase', name: 'liquibase-core', version: '2.0.2'
     runtime group: 'org.lilie.services.eliot', name: 'eliot-tice-dbmigration-all', version: "${versionTDBase}"
 
   }
