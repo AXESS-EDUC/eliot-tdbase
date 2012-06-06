6314282f64b59c786d778edc23a8bf194117bdc1
ticetime <github@ticetime.com>
2012-06-06 20:16:01 +0200

    #156 livraison 2.0.1-B1

--- eliot-tdbase/src/templates/eliot-tdbase-demo-config.groovy
+++ eliot-tdbase/src/templates/eliot-tdbase-demo-config.groovy
@@ -57,34 +57,41 @@ eliot.fichiers.maxsize.mega = 10
 // configuration des liens du menu portail et des annonces portail
 //
 eliot.portail.menu.affichage = true
-eliot.portail.menu.liens = [
-        [
-                url: "http://wwww.ticetime.com",
-                libelle: "ticetime"
-        ],
-        [
-                url: "https://github.com/ticetime/eliot-tdbase/wiki",
-                libelle: "eliot-tdbase sur Github"
-        ]
-]
-eliot.portail.news = [
-        "TDBase version ${appVersion} - Environnement DEMO",
+eliot.portail.menu.liens = [[url: "http://wwww.ticetime.com",
+        libelle: "ticetime"],
+        [url: "https://github.com/ticetime/eliot-tdbase/wiki",
+                libelle: "eliot-tdbase sur Github"]]
+eliot.portail.news = ["Environnement DEMO",
         "Le projet est disponible sur <a href=\"https://github.com/ticetime/eliot-tdbase/wiki\" target=\"_blank\">Github</a> !",
         "Login / mot de passe enseignant : ens1 / ens1",
         "Login / mot de passe eleve 1 : elv1 / elv1",
         "Login / mot de passe eleve 2 : elv2 / elv2",
-        "Login / mot de passe parent 1 : resp1 / resp1"
-]
+        "Login / mot de passe parent 1 : resp1 / resp1"]
 
 // set url documentation
-eliot.manuels.documents.urlMap = [
-        "${FonctionEnum.ENS.name()}": "http://ticetime.github.com/eliot-tdbase/aide/webhelp/Manuel_Utilisateur_TDBase_Enseignant/content/index.html",
+eliot.manuels.documents.urlMap = ["${FonctionEnum.ENS.name()}": "http://ticetime.github.com/eliot-tdbase/aide/webhelp/Manuel_Utilisateur_TDBase_Enseignant/content/index.html",
         "${FonctionEnum.DOC.name()}": "http://ticetime.github.com/eliot-tdbase/aide/webhelp/Manuel_Utilisateur_TDBase_Enseignant/content/index.html",
         "${FonctionEnum.CTR.name()}": "http://ticetime.github.com/eliot-tdbase/aide/webhelp/Manuel_Utilisateur_TDBase_Enseignant/content/index.html",
         "${FonctionEnum.DIR.name()}": "http://ticetime.github.com/eliot-tdbase/aide/webhelp/Manuel_Utilisateur_TDBase_Enseignant/content/index.html",
         "${FonctionEnum.ELEVE.name()}": "http://ticetime.github.com/eliot-tdbase/aide/webhelp/Manuel_Utilisateur_TDBase_Eleve/content/index.html",
-        "${FonctionEnum.PERS_REL_ELEVE.name()}": "http://ticetime.github.com/eliot-tdbase/aide/webhelp/Manuel_Utilisateur_TDBase_Paren/content/index.html"
-]
+        "${FonctionEnum.PERS_REL_ELEVE.name()}": "http://ticetime.github.com/eliot-tdbase/aide/webhelp/Manuel_Utilisateur_TDBase_Paren/content/index.html"]
+
+// Spécifie si les objets sensés être créés sont bien créés
+// à n'activier que si les données tdbase, notes et textes sont stockées dans
+// la même base
+eliot.interfacage.strongCheck = false
+// rest client config for textes
+eliot.webservices.rest.client.textes.user = "api"
+eliot.webservices.rest.client.textes.password = "api"
+eliot.webservices.rest.client.textes.urlServer = "http://www.ticeitime.com:8090"
+eliot.webservices.rest.client.textes.uriPrefix = "/eliot-textes/echanges/v2"
+eliot.webservices.rest.client.textes.connexionTimeout = 10000 // ms
+// rest client config for notes
+eliot.webservices.rest.client.notes.user = "api"
+eliot.webservices.rest.client.notes.password = "api"
+eliot.webservices.rest.client.notes.urlServer = "http://www.ticeitime.com:8090"
+eliot.webservices.rest.client.notes.uriPrefix = "/eliot-notes/echanges/v2"
+eliot.webservices.rest.client.notes.connexionTimeout = 10000 // ms
 
 // data source
 dataSource {
@@ -96,3 +103,34 @@ dataSource {
   logSql = false
 }
 
+log4j = {
+
+  appenders {
+    //file name:'file', file:'/appli/tomcat/logs/eliot-tdbase-app.log'
+    file name: 'file', file: '/tmp/eliot-tdbase-app.log'
+  }
+
+  root {
+    info 'file'
+    additivity = true
+  }
+
+  error 'org.hibernate.type', 'org.springframework.security'
+
+  error 'grails',
+        'org.codehaus.groovy.grails.web.servlet',  //  controllers
+        'org.codehaus.groovy.grails.web.pages', //  GSP
+        'org.codehaus.groovy.grails.web.sitemesh', //  layouts
+        'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
+        'org.codehaus.groovy.grails.web.mapping', // URL mapping
+        'org.codehaus.groovy.grails.commons', // core / classloading
+        'org.codehaus.groovy.grails.plugins',
+        'org.codehaus.groovy.grails.orm.hibernate' // plugins
+  //		'org.springframework'
+
+  warn 'org.mortbay.log'
+  info 'grails.app'
+  error 'org.lilie.services.eliot.tice.webservices.rest.client.RestClient'
+  error file: "StackTrace"
+
+}
