9ff2daf3204613fb0fadf80d2e7387d3456e89c7
ticetime <github@ticetime.com>
2012-06-12 09:19:40 +0200

    #157 Gestion de dépendances pour le déploiement automatique sur Jenkins

--- eliot-tice-plugin/grails-app/conf/BuildConfig.groovy
+++ eliot-tice-plugin/grails-app/conf/BuildConfig.groovy
@@ -68,6 +68,7 @@ grails.project.dependency.resolution = {
   plugins {
 
     build(":tomcat:$grailsVersion",
+          ":rest-client-builder:1.0.2",
           ":release:2.0.2") {
       export = false
     }
