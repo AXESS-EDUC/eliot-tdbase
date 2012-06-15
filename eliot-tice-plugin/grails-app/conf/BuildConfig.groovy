7114ced5c6528f5cd24389c585fc0fceea82c511
ticetime <github@ticetime.com>
2012-06-15 11:39:25 +0200

    #163 release 2.0.2-RC1 [fixed #163] passage en version 2.0.2-SNAPSHOT

--- eliot-tice-plugin/grails-app/conf/BuildConfig.groovy
+++ eliot-tice-plugin/grails-app/conf/BuildConfig.groovy
@@ -33,7 +33,7 @@ grails.project.test.reports.dir = "target/test-reports"
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 
-versionTDBase = "2.0.2-RC1"
+versionTDBase = "2.0.2-SNAPSHOT"
 
 grails.project.dependency.resolution = {
 
