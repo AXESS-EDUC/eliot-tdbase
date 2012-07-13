f792bd76335f10503bf254906b7cf80aff9c5f5c
ticetime <github@ticetime.com>
2012-07-13 14:36:02 +0200

    #170 préparation liv. 2.0.3-RC1

--- eliot-tice-plugin/grails-app/conf/BuildConfig.groovy
+++ eliot-tice-plugin/grails-app/conf/BuildConfig.groovy
@@ -33,7 +33,7 @@ grails.project.test.reports.dir = "target/test-reports"
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 
-versionTDBase = "2.0.2-SNAPSHOT"
+versionTDBase = "2.0.3-RC1"
 
 grails.project.dependency.resolution = {
 
