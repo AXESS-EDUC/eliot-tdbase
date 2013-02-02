fd883efd8a889047b40f07796094cb7ff45775b9
ticetime <github@ticetime.com>
2013-02-02 14:18:43 +0100

    => 2.0.5-SNAPSHOT

--- eliot-tice-plugin/grails-app/conf/BuildConfig.groovy
+++ eliot-tice-plugin/grails-app/conf/BuildConfig.groovy
@@ -33,7 +33,7 @@ grails.project.test.reports.dir = "target/test-reports"
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 
-versionTDBase = "2.0.5-RC2"
+versionTDBase = "2.0.5-SNAPSHOT"
 
 grails.project.dependency.resolution = {
 
