c6901e3290c6862521036bbfe04e3932292dc2b9
jtra <john.tranier@axess.fr>
2013-04-22 16:52:01 +0200

    2.1.0-SNAPSHOT => 2.1.0-S14-eliot-2.9.1-B2

--- eliot-tice-plugin/grails-app/conf/BuildConfig.groovy
+++ eliot-tice-plugin/grails-app/conf/BuildConfig.groovy
@@ -33,7 +33,7 @@ grails.project.test.reports.dir = "target/test-reports"
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 
-versionTDBase = "2.1.0-SNAPSHOT"
+versionTDBase = "2.1.0-S14-eliot-2.9.1-B2"
 
 grails.project.dependency.resolution = {
   checksums(false)
