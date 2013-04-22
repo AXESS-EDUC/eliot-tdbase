e0e62738d885142e00cae48883f0fbf0a695e9e0
jtra <john.tranier@axess.fr>
2013-04-22 16:50:52 +0200

    2.0.5-SNAPSHOT => 2.1.0-SNAPSHOT

--- eliot-tice-plugin/grails-app/conf/BuildConfig.groovy
+++ eliot-tice-plugin/grails-app/conf/BuildConfig.groovy
@@ -33,7 +33,7 @@ grails.project.test.reports.dir = "target/test-reports"
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 
-versionTDBase = "2.0.5-SNAPSHOT"
+versionTDBase = "2.1.0-SNAPSHOT"
 
 grails.project.dependency.resolution = {
   checksums(false)
