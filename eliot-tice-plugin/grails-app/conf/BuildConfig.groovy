6e010387bcc398359aaf27dd33af27e4f06f29fa
jtra <john.tranier@axess.fr>
2013-05-15 10:31:50 +0200

    2.0.5-eliot-2.9.1-RC2 => 2.0.5-eliot-2.9.1-RC5

--- eliot-tice-plugin/grails-app/conf/BuildConfig.groovy
+++ eliot-tice-plugin/grails-app/conf/BuildConfig.groovy
@@ -33,7 +33,7 @@ grails.project.test.reports.dir = "target/test-reports"
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 
-versionTDBase = "2.0.5-eliot-2.9.1-RC2"
+versionTDBase = "2.0.5-eliot-2.9.1-RC5"
 
 grails.project.dependency.resolution = {
   checksums(false)
