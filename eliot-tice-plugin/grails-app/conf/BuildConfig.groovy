f6f18a9fb69c6d61971267c6c0a009d4364ddae3
John Tranier <john.tranier@ticetime.com>
2013-09-26 09:03:44 +0200

    Correction changement version 2.2.0-SNAPSHOT => 2.3.0-SNAPSHOT

--- eliot-tice-plugin/grails-app/conf/BuildConfig.groovy
+++ eliot-tice-plugin/grails-app/conf/BuildConfig.groovy
@@ -33,7 +33,7 @@ grails.project.test.reports.dir = "target/test-reports"
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 
-versionTDBase = "2.2.0-SNAPSHOT"
+versionTDBase = "2.3.0-SNAPSHOT"
 
 grails.project.dependency.resolution = {
   checksums(false)
