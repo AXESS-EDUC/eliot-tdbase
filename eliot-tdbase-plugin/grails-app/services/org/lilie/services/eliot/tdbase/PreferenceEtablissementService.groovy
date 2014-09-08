5dffe6bd2ab6f8f8b7932d5e0149d397007fd548
Franck Silvestre <franck.silvestre@ticetime.com>
2014-09-08 15:50:56 +0200

    TDB-88 ajout de la classe PreferenceEtablissement et du service associe ; methode de recuperation et de creation testee

---
+++ eliot-tdbase-plugin/grails-app/services/org/lilie/services/eliot/tdbase/PreferenceEtablissementService.groovy
@@ -0,0 +1,35 @@
+package org.lilie.services.eliot.tdbase
+
+import org.lilie.services.eliot.tice.annuaire.Personne
+import org.lilie.services.eliot.tice.scolarite.Etablissement
+import org.lilie.services.eliot.tice.utils.contract.ContractService
+
+/**
+ * Service de gestion des préférences établissement
+ * @author Franck Silvestre
+ */
+class PreferenceEtablissementService {
+
+    ContractService contractService
+
+    /**
+     * Récupère l'objet correspondant aux préférences d'un établissement
+     * @param personne la personne effectuant la demande
+     * @param role le role applicatif de la personne effectuant la demande
+     * @param etablissement
+     * @return l'objet correspondant aux préférences de l'établissement
+     */
+    PreferenceEtablissement getPreferenceForEtablissement(Personne personne,
+                                                          Etablissement etablissement,
+                                                          RoleApplicatif role = null) {
+
+        PreferenceEtablissement pref = PreferenceEtablissement.findByEtablissement(etablissement)
+        if (!pref && role == RoleApplicatif.ADMINISTRATEUR) {
+            pref = new PreferenceEtablissement(etablissement: etablissement,
+                    lastUpdateAuteur: personne,
+                    mappingFonctionRole: "{}")
+            pref.save(failOnError: true)
+        }
+        pref
+    }
+}
