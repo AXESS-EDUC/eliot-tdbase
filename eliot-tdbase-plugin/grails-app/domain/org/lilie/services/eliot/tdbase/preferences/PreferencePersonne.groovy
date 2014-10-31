ff8838128b2bc57b1090fbb07db343607bfd2e0d
Franck Silvestre <franck.silvestre@ticetime.com>
2014-10-31 17:41:56 +0100

    TDB-74 Ajout classe domaine PreferenceUtilisateur

---
+++ eliot-tdbase-plugin/grails-app/domain/org/lilie/services/eliot/tdbase/preferences/PreferencePersonne.groovy
@@ -0,0 +1,30 @@
+package org.lilie.services.eliot.tdbase.preferences
+
+import org.lilie.services.eliot.tice.annuaire.Personne
+import org.lilie.services.eliot.tice.scolarite.Etablissement
+
+
+/**
+ * Classe qui représente les préférences d'un établissement : concerne
+ * notamment le mapping fonction/role applicatif au sein de TD Base
+ */
+class PreferencePersonne {
+
+    Personne personne
+
+    String notifications
+    Date lastUpdated
+
+    static constraints = {
+
+    }
+
+
+
+    static mapping = {
+        table('td.preference_personne')
+        id(column: 'id', generator: 'sequence', params: [sequence: 'td.preference_personne_id_seq'])
+        cache(true)
+    }
+}
+
