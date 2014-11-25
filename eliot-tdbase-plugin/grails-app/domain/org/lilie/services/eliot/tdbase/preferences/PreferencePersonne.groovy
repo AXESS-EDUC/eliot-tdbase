743cce8f8fb1f676f617512b9f8c2a66721e02f9
Franck Silvestre <franck.silvestre@ticetime.com>
2014-11-25 16:52:31 +0100

    TDB-126 Ajout du choix du moyen de notification (email ou sms) - modification de la classe PreferencePersonne

--- eliot-tdbase-plugin/grails-app/domain/org/lilie/services/eliot/tdbase/preferences/PreferencePersonne.groovy
+++ eliot-tdbase-plugin/grails-app/domain/org/lilie/services/eliot/tdbase/preferences/PreferencePersonne.groovy
@@ -14,11 +14,15 @@ class PreferencePersonne {
 
     Boolean notificationOnCreationSeance = false
     Boolean notificationOnPublicationResultats = false
+    Integer codeSupportNotification
 
     Date lastUpdated
 
     static constraints = {
-
+        codeSupportNotification nullable: true, inList: SupportNotification.values()*.code, validator: { val, obj ->
+            return (val != null && (obj.notificationOnCreationSeance || obj.notificationOnPublicationResultats) ||
+                    (val == null && !obj.notificationOnCreationSeance && !obj.notificationOnPublicationResultats))
+        }
     }
 
     static mapping = {
@@ -26,5 +30,19 @@ class PreferencePersonne {
         id(column: 'id', generator: 'sequence', params: [sequence: 'td.preference_personne_id_seq'])
         cache(true)
     }
+
+}
+
+enum SupportNotification {
+    E_MAIL(1),
+    SMS(2),
+    E_MAIL_AND_SMS(3)
+
+    int code
+
+    SupportNotification(int code) {
+        this.code = code
+    }
+
 }
 
--- eliot-tdbase-plugin/test/unit/org/lilie/services/eliot/tdbase/preferences/PreferencePersonneSpec.groovy
+++ eliot-tdbase-plugin/test/unit/org/lilie/services/eliot/tdbase/preferences/PreferencePersonneSpec.groovy
@@ -4,6 +4,7 @@ import grails.test.mixin.TestMixin
 import grails.test.mixin.support.GrailsUnitTestMixin
 import org.lilie.services.eliot.tice.annuaire.Personne
 import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
  * See the API for {@link grails.test.mixin.support.GrailsUnitTestMixin} for usage instructions
@@ -20,12 +21,36 @@ class PreferencePersonneSpec extends Specification {
     }
 
     void "la création d'une préférence personne valide initialise l'abonnement des notifications"() {
-        when:"une nouvelle préférence personne est crée"
+        when: "une nouvelle préférence personne est crée"
         preferencePersonne = new PreferencePersonne(personne: Mock(Personne))
 
-        then:"les abonnements aux notifications ne pas sont activés"
+        then: "les abonnements aux notifications ne pas sont activés"
         !preferencePersonne.notificationOnCreationSeance
         !preferencePersonne.notificationOnPublicationResultats
+        preferencePersonne.codeSupportNotification == null
 
     }
+
+    @Unroll
+    void "lorsqu'une notification est active au moins, le code support notification est renseigné correctement"() {
+        given: "une préférence personne"
+        preferencePersonne = new PreferencePersonne(personne: Mock(Personne))
+
+        when: "les préférences sont modifiées avec au moins une notification active"
+        preferencePersonne.notificationOnPublicationResultats = notifPubliRes
+        preferencePersonne.notificationOnCreationSeance = notifCreatSean
+        preferencePersonne.codeSupportNotification = codeSupp
+
+        then: "les préférences sont valides que si le code support est correctement paramétré"
+        preferencePersonne.validate() == prefIsValide
+
+        where:
+        notifPubliRes | notifCreatSean | codeSupp                        | prefIsValide
+        false         | false          | null                            | true
+        true          | false          | null                            | false
+        true          | false          | 0                               | false // 0 n'est pas un code valide
+        true          | false          | SupportNotification.E_MAIL.code | true
+
+    }
+
 }
