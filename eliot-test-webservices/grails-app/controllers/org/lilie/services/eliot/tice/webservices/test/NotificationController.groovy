f397d55b87fbd699ced496faa1f258c72a322ffd
Franck Silvestre <franck.silvestre@ticetime.com>
2014-12-10 11:44:13 +0100

    fixed TDB-129 Creation du mock du web service de notification - initialisation

--- eliot-test-webservices/grails-app/controllers/org/lilie/services/eliot/tice/webservices/test/NotificationController.groovy
+++ eliot-test-webservices/grails-app/controllers/org/lilie/services/eliot/tice/webservices/test/NotificationController.groovy
@@ -1,5 +1,8 @@
 package org.lilie.services.eliot.tice.webservices.test
 
+import groovy.json.JsonSlurper
+import org.codehaus.groovy.runtime.powerassert.PowerAssertionError
+
 class NotificationController {
 
 
@@ -8,7 +11,7 @@ class NotificationController {
     def creerNotification() {
         String notification = params.notification
         String resp = '''{"success":true}'''
-        if (!notificationisValide()) {
+        if (!notificationisValide(notification)) {
             resp = '''{"success":false,"message":"error_notification"}'''
 
         }
@@ -16,7 +19,24 @@ class NotificationController {
     }
 
 
-    private boolean notificationisValide() {
-        true
+    private boolean notificationisValide(String notificationAsString) {
+        boolean res = true
+        def jsonSlurper = new JsonSlurper()
+        Map notification
+        try {
+            notification = jsonSlurper.parseText(notificationAsString)
+            assert notification.etablissementIdExterne != null
+            assert notification.demandeurIdExterne != null
+            assert notification.message != null
+            assert notification.destinatairesIdExterne.size() > 0
+            assert (notification.supports.contains('EMAIL') || notification.supports.contains('SMS'))
+        } catch (IllegalArgumentException iae) {
+            log.error(iae.message)
+            res = false
+        } catch (PowerAssertionError pae) {
+            log.error(pae.message)
+            res = false
+        }
+        res
     }
 }
