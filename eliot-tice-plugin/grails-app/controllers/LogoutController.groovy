b0b9f1830216f28ef3a0129ac90d21f8f0f15d40
John Tranier <john.tranier@ticetime.com>
2015-07-09 15:22:51 +0200

    TDB-253 - 0045614: Le verrou d'un sujet n'est pas libéré si l'utilisateur quitte sa session en se déconnectant

--- eliot-tice-plugin/grails-app/controllers/LogoutController.groovy
+++ eliot-tice-plugin/grails-app/controllers/LogoutController.groovy
@@ -1,4 +1,5 @@
 import org.codehaus.groovy.grails.plugins.springsecurity.SpringSecurityUtils
+import org.lilie.services.eliot.tice.Attachement
 
 class LogoutController {
 
@@ -9,6 +10,32 @@ class LogoutController {
    */
   def index() {
     // TODO  put any pre-logout code here
+
+    libereAllVerrou()
     redirect uri: SpringSecurityUtils.securityConfig.logout.filterProcessesUrl // '/j_spring_security_logout'
+  }
+
+  /**
+   * Supprime tous les verrous détenus par cet utilisateurs sur les sujets & items
+   */
+  private void libereAllVerrou() {
+    // Note : Le choix de classe du domaine Attachement est arbitraire ... Il s'agit juste d'exécuter une requête HQL
+    Attachement.executeUpdate("""
+        UPDATE Sujet s
+        SET auteurVerrou=NULL,
+          dateVerrou=NULL
+        WHERE s.auteurVerrou=:auteur
+      """,
+        [auteur: authenticatedPersonne]
+    )
+
+    Attachement.executeUpdate("""
+        UPDATE Question q
+        SET auteurVerrou=NULL,
+          dateVerrou=NULL
+        WHERE q.auteurVerrou=:auteur
+      """,
+        [auteur: authenticatedPersonne]
+    )
   }
 }
