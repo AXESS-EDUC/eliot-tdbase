159f6d58f41d7920876d622dffd00f339dcb963a
bahj <bouchra.bellach@axess.fr>
2015-10-22 11:01:38 +0200

    2.4.2-SNAPSHOT => 2.4.5-eliot-3.7.2-RC1

--- eliot-notes-plugin/EliotNotesPluginGrailsPlugin.groovy
+++ eliot-notes-plugin/EliotNotesPluginGrailsPlugin.groovy
@@ -30,7 +30,7 @@ class EliotNotesPluginGrailsPlugin {
   // the group id
   def groupId = "org.lilie.services.eliot"
   // the plugin version
-  def version = "2.4.2-SNAPSHOT"
+  def version = "2.4.5-eliot-3.7.2-RC1"
   // the version or versions of Grails the plugin is designed for
   def grailsVersion = "2.0.1 > *"
   // the other plugins this plugin depends on
