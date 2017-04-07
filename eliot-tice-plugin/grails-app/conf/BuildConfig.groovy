a2c15db43cb38a585a120e3db0c8b89d56c4c088
Alicia Giacomazzo <alicia.giacomazzo@axess.fr>
2017-04-07 11:11:14 +0200

    3.0.0-SNAPSHOT => 3.0.0-RC1-eliot-4.4.0

--- eliot-tice-plugin/grails-app/conf/BuildConfig.groovy
+++ eliot-tice-plugin/grails-app/conf/BuildConfig.groovy
@@ -33,7 +33,7 @@ grails.project.test.reports.dir = "target/test-reports"
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 
-versionTDBase = "3.0.0-SNAPSHOT"
+versionTDBase = "3.0.0-RC1-eliot-4.4.0"
 
 grails.plugin.location.'eliot-competence-plugin' = "../eliot-competence-plugin"
 
